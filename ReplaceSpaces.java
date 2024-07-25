2d60e8bb48bafff29f5ea68b99e5fd58eb2f1d95
Kishori Khatke <123355545+Kishorikhatke22@users.noreply.github.com>
2024-07-25 16:05:47 +0530

    Add files via upload

--- ReplaceSpaces.java
+++ ReplaceSpaces.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 public class ReplaceSpaces {
     public static void main(String[] args) {
     
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a sentence:");
-        String input = scanner.nextLine();
+        String a = sc.nextLine();
+        int l = a.length();
+        String f = "";
+        for(int i=0;i<1;i++)
+        {
+            char c = a.charAt(i);
+            if(c!=' ')
+            f = f+c;
+        }
 
-        
-        String output = input.trim().replaceAll("\\s+", "#");
 
     
-        System.out.println("Modified sentence: " + output);
+        System.out.println("sentence with all white space removes:" +f);
     }
 }
